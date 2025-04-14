685ba5418b7ec297b6cfaf5ec5e137e52843f33b
Vishal Kumar Sharma <vishalkumar.sharma37@gmail.com>
2025-04-14 21:24:31 +0530

    Sitemap included

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.multigyan.in',
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: 'daily',
+    priority: 0.7,
+  }
+  
